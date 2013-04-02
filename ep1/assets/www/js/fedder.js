e83749e9a05182658de733e1b7774a926c5b7273
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2013-04-02 10:47:16 -0300

    Agora tem checkConnection.

--- ep1/assets/www/js/fedder.js
+++ ep1/assets/www/js/fedder.js
@@ -1,33 +1,57 @@
 var fedder = {
 
+	checkConnection: function() {
+		var networkState = navigator.connection.type;
+		if (networkState == Connection.NONE) {
+			return false;
+		}
+		return true;
+	},
+
 	loadFeeds: function() {
-		var feed = new google.feeds.Feed("http://www.imprensa.usp.br/?feed=rss2");
+		if (fedder.checkConnection) {
+			fedder.updateFeeds();
+		}
+	},
+
+	updateFeeds: function() {
+		var feed = new google.feeds.Feed(feedURL);
 		feed.includeHistoricalEntries();
 		feed.setNumEntries(10);
 
 		feed.load(function(result) {
 			if (!result.error) {
-				$("ul").append('<li data-role="divider" data-theme="b">'+result.feed.title+'<span class="ui-li-count">'+result.feed.entries.length+'</span></li>');
 				console.log(result.feed);
 				for (var i = 0; i < result.feed.entries.length; i++) {
 					var entradaDoFeed = result.feed.entries[i];
 					var dataPublicacao = entradaDoFeed.publishedDate;
-					$("ul").append('<li><a href="'+entradaDoFeed.link+'"><h2>'+entradaDoFeed.title+'</h2><p>'+entradaDoFeed.contentSnippet+'</p><p class="ui-li-aside"><strong>Publicação: '+dataPublicacao.substring(0, dataPublicacao.length - 7)+'</strong>PM</p></a></li>');
-					fedder.store.addNewFeed(result.feed.feedUrl, entradaDoFeed);
+					fedder.store.addNewFeed(feedURL, entradaDoFeed);
 				}
 			}
+			fedder.putFeedsOnHtml();
+		});
+	},
+
+	putFeedsOnHtml: function() {
+		fedder.store.findByFeedURL(feedURL, function(result) {
+			$("ul").append('<li data-role="divider" data-theme="b">'+result[0].feedURL+'<span class="ui-li-count">'+result.length+'</span></li>');
+			console.log(result);
+			for (var i = 0; i < result.length; i++) {
+				var entradaDoFeed = result[i];
+				var dataPublicacao = entradaDoFeed.publishedDate;
+				$("ul").append('<li><a href="'+entradaDoFeed.link+'"><h2>'+entradaDoFeed.title+'</h2><p>'+entradaDoFeed.contentSnippet+'</p><p class="ui-li-aside"><strong>Publicação: '+dataPublicacao.substring(0, dataPublicacao.length - 7)+'</strong>PM</p></a></li>');
+			}
 			$('ul').listview('refresh');
-			fedder.store.findByFeedURL("http://www.imprensa.usp.br/?feed=rss2", function(feed) {
-				console.log(feed);
-			});
 		});
 	},
 
 	initialize: function() {
 		this.store = new Storage();
 		google.load("feeds", "1");
-		google.setOnLoadCallback(fedder.loadFeeds);
+		google.setOnLoadCallback(fedder.updateFeeds);
 	}
 };
 
+var feedURL = "http://www.imprensa.usp.br/?feed=rss2";
 fedder.initialize();
+window.setInterval(fedder.loadFeeds,30000);
